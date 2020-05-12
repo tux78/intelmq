9d09df9b14fe445db89435a0db6655618502cd5c
tux78 <vdberge.sebastian@googlemail.com>
2020-05-12 21:04:36 +0200

    Python3 update ESM IP output bot

--- intelmq/bots/outputs/mcafee/output_esm_ip.py
+++ intelmq/bots/outputs/mcafee/output_esm_ip.py
@@ -13,22 +13,32 @@ field: field from IntelMQ message to extract (e.g. destination.ip)
 
 from intelmq.lib.bot import Bot
 from intelmq.lib.exceptions import MissingDependencyError
+import json
 
 try:
-    from mfe_saw.esm import ESM
+    import requests
 except ImportError:
-    ESM = None
-
+    requests = None
+try:
+    import base64
+except ImportError:
+    base64 = None
 
 class ESMIPOutputBot(Bot):
 
     def init(self):
-        if ESM is None:
-            raise MissingDependencyError("mfe_saw")
+        if requests is None:
+            raise MissingDependencyError("requests")
+        if base64 is None:
+            raise MissingDependencyError("base64")
 
-        self.esm = ESM()
+        self.esm = ESM(
+            self.parameters.esm_ip,
+            self.parameters.esm_user,
+            self.parameters.esm_password
+        )
         try:
-            self.esm.login(self.parameters.esm_ip, self.parameters.esm_user, self.parameters.esm_password)
+            self.esm.login()
         except Exception:
             raise ValueError('Could not Login to ESM.')
 
@@ -37,7 +47,7 @@ class ESMIPOutputBot(Bot):
         try:
             retVal = self.esm.post('sysGetWatchlists?hidden=false&dynamic=false&writeOnly=false&indexedOnly=false',
                                    watchlist_filter)
-            for WL in retVal:
+            for WL in retVal.json():
                 if (WL['name'] == self.parameters.esm_watchlist):
                     self.watchlist_id = WL['id']
         except TypeError:
@@ -46,16 +56,64 @@ class ESMIPOutputBot(Bot):
 
     def process(self):
         event = self.receive_message()
-        self.logger.info('Message received.')
         try:
-            self.esm.post('sysAddWatchlistValues', {'watchlist': {'value': self.watchlist_id},
-                                                    'values': '["' + event.get(self.parameters.field) + '"]'},
-                          raw=True)
-            self.logger.info('ESM Watchlist updated')
-            self.acknowledge_message()
+            payload = {
+                'watchlist': self.watchlist_id,
+                'values': '["' + event.get(self.parameters.field) + '"]'
+            }
+            retval = self.esm.sysAddWatchlistValues(payload)
+            self.logger.info('ESM Watchlist updated: ' + event.get(self.parameters.field))
         except Exception:
             self.logger.exception('Error when updating watchlist.')
         self.acknowledge_message()
 
+class ESM ():
+
+    headers = {}
+
+    def __init__ (self, esm_ip, esm_user, esm_pw):
+        self.url = "https://{}/rs/esm/v2/".format(esm_ip)
+        self.user = base64.b64encode(esm_user.encode('utf-8')).decode()
+        self.pw = base64.b64encode(esm_pw.encode('utf-8')).decode()
+
+        self.headers = {
+            'content-type': 'application/json'
+        }
+
+    def login (self):
+
+        payload = {
+            'username': self.user,
+            'password': self.pw,
+            'locale': 'en_US',
+            'os': 'Win32'
+        }
+        response = self.post ('login', payload)
+        self.headers['X-Xsrf-Token'] = response.headers['Xsrf-Token']
+        self.headers['Cookie'] = response.headers['Set-Cookie']
+
+    def logout (self):
+        response = self.post ('logout', '')
+        del self.headers['X-Xsrf-Token']
+        del self.headers['Cookie']
+
+    def sysAddWatchlistValues (self, payload):
+
+        response = self.post ('sysAddWatchlistValues', payload)
+        return payload
+
+    def post (self, method, payload):
+
+        if 'X-Xsrf-Token' not in self.headers and method != 'login':
+            print (self.headers)
+            raise ValueError('You have to login first.')
+
+        response = requests.post(
+            self.url + method,
+            headers = self.headers,
+            data = json.dumps (payload),
+            verify = False
+        )
+        return response
 
 BOT = ESMIPOutputBot
